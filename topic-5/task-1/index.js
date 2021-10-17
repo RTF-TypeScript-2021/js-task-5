eb1847680ec486bea1d48083cf950e95f7ce5f25
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2021-10-17 19:31:56 +0500

    tasks 1..2

--- topic-5/task-1/index.js
+++ topic-5/task-1/index.js
@@ -1,17 +1,14 @@
-/**
-Не пускай джуна в прод.
-В функции что он написал, закралась ошибка.
-По описанию наших аналитиков, подсчет голубиных крошек на полу должен отрисовываться плавно.
-А по итогу.... Лаги, сильный фриз, и только потом, итог исполнения функции.
-Кажется, настало твое время, мой друг
-*/
-
+"use strict"
 
 function breadcrumbCalculate(setter){
-    for (let i = 0; i < 1e7; i++) {
+    let i = 0;
+    const timer = setInterval(() => {
         i++;
-        setter(i);
-    }
+        setter(i)
+        if (i === 1e7) {
+            clearInterval(timer);
+        }
+    }, 5)
 }
 
 module.exports.breadcrumbCalculate = breadcrumbCalculate;
--- topic-5/task-2/index.js
+++ topic-5/task-2/index.js
@@ -1,19 +1,44 @@
+"use strict"
+
+const { stringOfPurchases } = require("./list-items");
+
 /**
-    Поход на закупки по выходным - святое дело.
-    Денег нет, сколько потратили понятно... А вот на что...
-    В вашей директории лежит файлик с моими покупками и кодами МСС.
-    Напиши функцию которая вернет массив покупок (Purchase)
-    Класс должен иметь поля: code, type, value, name.
-    type - наименование группы по МСС, а value сумма затраченная на приобритение данного продукта.
+ * @param {String} string 
+ */
+function* resolveBudget(string = stringOfPurchases) {
+    const orders = string
+        .split(",")
+        .map(order => order.trim().split(' '));
+    for(const order of orders) {
+        const value = parseFloat(order.pop());
+        const mcc = parseInt(order.pop());
+        const name = order.join(" ");
 
-    Как строить класс и как экспортиоровать функцию resolveBudget, дело ваше, полная свобода.
-*/
-
-function resolveBudget(string){
-    
+        yield new Purchase(mcc, MCC.prototype.keywords[mcc], value, name);
+    }
 }
 
-class Purchase{
+class Purchase {
+    constructor(code, type, value, name) {
+        this.code = code;
+        this.type = type;
+        this.value = value;
+        this.name = name;
+    }
+}
+
+class MCC {
+    //static keycodes = {...} <- eslint блокирует?
+}
+
+MCC.prototype.keywords = {
+    5411: "Продуктовые магазины, супермаркеты",
+    5732: "Магазины электро-товаров",
+    5812: "Рестораны, места общественного питания",
+    5993: "Табачные магазины",
+    5039: "Строительные материалы",
+    5172: "Нефть и нефтепродукты",
+    5651: "Семейные магазины одежды"
 }
 
 module.exports.resolveBudget = resolveBudget;

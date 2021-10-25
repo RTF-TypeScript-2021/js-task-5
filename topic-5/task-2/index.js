640cc88f2908cbeaa584009f595221a1b848573b
URFUTEAM <european.ass@gmail.com>
2021-10-26 00:04:42 +0500

    done all tasks

--- topic-5/task-2/index.js
+++ topic-5/task-2/index.js
@@ -8,12 +8,37 @@
 
     Как строить класс и как экспортиоровать функцию resolveBudget, дело ваше, полная свобода.
 */
-
+const stringOfPurchases = require("./list-items");
 function resolveBudget(string){
-    
+    const listPurchases = [];
+    for(const s of stringOfPurchases.split(",")){
+        listPurchases.push(new Purchase(s));
+    }
+
+    return listPurchases;
 }
 
+const typesArray = {
+        5411: 'MAGNIT',
+        5732: 'DNS',
+        5812: 'STARBUCKS', 
+        5993: 'BAR',
+        5039: 'LEROY MERLIN',
+        5172: 'Lukoil',
+        5651: 'ADIDAS'
+};
+
 class Purchase{
+    constructor(name, code, value, type) {
+        if (typeof name !== 'string' || typeof code !== 'number' || 
+        typeof type !== 'string' || typeof value !== 'number') {
+            throw UserException("Invalid data")
+        }
+        this.name = name;
+        this.code = code;
+        this.type = type;
+        this.value = value;
+    }  
 }
 
 module.exports.resolveBudget = resolveBudget;
